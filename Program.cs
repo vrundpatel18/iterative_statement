c26fc5c91ce619fc54c56733087102ca865b2a9f
5541 <5541@DESKTOP-2R6N1UC>
2023-01-30 11:44:46 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,60 @@
+﻿// Vrund Patel
+// 1/29/23
+// iterative statement assignment
+using System.ComponentModel.Design.Serialization;
+
+namespace iterative_statement
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int input = 0;
+            string series = "";
+            
+            while(true)
+            {
+                Console.WriteLine("Enter an integer number between 1 and 100: ");
+                input = int.Parse(Console.ReadLine());
+
+                if (input >= 1 && input <=100)
+                {
+                    break;
+                }
+            }
+
+             while(true)
+             {
+                Console.WriteLine("Specify the series type as even or odd: ");
+                series = Console.ReadLine();
+
+                if (series == "even" || series == "odd")
+                {
+                    break;
+                }
+             }
+
+                if (series == "odd")
+                {
+                    Console.WriteLine("You have selected " + series + " series. The numbers between 0 and " + input + " are: ");
+                    for (int i = 1; i <= input; i = i + 2)
+                    {
+                        Console.WriteLine(i);
+                    }
+                }
+
+                if (series == "even")
+                {
+                    Console.WriteLine("You have selected " + series + " series. The numbers between 0 and " + input + " are: ");
+                    for (int i = 0; i <= input; i = i + 2)
+                    {
+                        Console.WriteLine(i);
+                    }
+                }
+            
+            
+
+        }
+    }
+}
